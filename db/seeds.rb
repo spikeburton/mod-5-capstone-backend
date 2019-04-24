ecd07cd15367f19cd77f447fa8237b77f3c99b69
spikeburton <ralphburton3@gmail.com>
2019-04-24 16:10:38 -0400

    add rest-client to Gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+API = "https://raw.githubusercontent.com/spikeburton/scenic-byway-api/master/db.json"
 
 User.create(username: "darryl", password: "p@ssw0rd", email: "darryl.figtree@gmail.com", first_name: "Darryl", last_name: "Fiddle-Leaf")
+
+data = JSON.parse(RestClient.get(API))
+drives = data["byways"]["GA"]
