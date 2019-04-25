1640de4bb7ef93363a27c1179db7dbd15b3f30ed
spikeburton <ralphburton3@gmail.com>
2019-04-24 21:36:08 -0400

    implement jwt auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,36 @@
 class ApplicationController < ActionController::API
+  # before_action :authorize!
+
+  def encode_token(payload)
+    JWT.encode(payload, ENV['SECRET'], 'HS256')
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, ENV['SECRET'])[0]
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def current_user
+    if decoded_token
+      @user = User.find(decoded_token['user_id'])
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorize!
+    render json: { error: "Must be logged in" }, status: :unauthorized unless logged_in?
+  end
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,9 +1,12 @@
 class AuthController < ApplicationController
+  # skip_before_action :authorize!, only: [:login]
+
   def login
     @user = User.find_by(username: login_params[:username])
     if @user && @user.authenticate(login_params[:password])
       # login user
-      binding.pry
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, jwt: token }, status: :accepted
     else
       render json: { errors: ["Invalid username or password"] }, status: :unauthorized
     end
