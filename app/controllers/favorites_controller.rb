48929b3eafdb21172b5df62fc37c6efd6cc896f7
spikeburton <ralphburton3@gmail.com>
2019-04-25 10:51:17 -0400

    add custom json for favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,4 +5,20 @@ class FavoritesController < ApplicationController
     @favorites = Favorite.all
     render json: @favorites.all_json
   end
+
+  def create
+    @favorite = Favorite.new(favorite_params)
+    if @favorite && @favorite.valid?
+      @favorite.save
+      render json: @favorite, status: :created
+    else
+      render json: { errors: @favorite.errors.full_messages }, status: :not_accepted
+    end
+  end
+
+  private
+
+  def favorite_params
+    params.permit(:drive_id, :user_id)
+  end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,7 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :drive
 
-  def to_json
+  def to_custom_json
     {
       user_id: self.user_id,
       drive_id: self.drive_id
@@ -10,6 +10,6 @@ class Favorite < ApplicationRecord
   end
 
   def self.all_json
-    Favorite.all.collect(&:to_json)
+    Favorite.all.collect(&:to_custom_json)
   end
 end
