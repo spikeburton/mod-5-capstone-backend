9874a7eb922f0d4b433ce271a37f1522c4781337
spikeburton <ralphburton3@gmail.com>
2019-04-24 21:47:01 -0400

    render custom json for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   has_secure_password
   has_many :favorites
   has_many :drives, through: :favorites
+
+  def to_json
+    {
+      id: self.id,
+      username: self.username,
+      email: self.email,
+      first_name: self.first_name,
+      last_name: self.last_name
+    }
+  end
 end
