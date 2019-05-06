1e613ace2c5b2c1d8b6da40b9a1f06a7a8ad5795
spikeburton <ralphburton3@gmail.com>
2019-05-06 15:54:15 -0400

    add validations to create a drive

--- app/models/drive.rb
+++ app/models/drive.rb
@@ -1,4 +1,12 @@
 class Drive < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :state, presence: true
+  validates :bound_a_lng, presence: true
+  validates :bound_a_lat, presence: true
+  validates :bound_b_lng, presence: true
+  validates :bound_b_lat, presence: true
 end
